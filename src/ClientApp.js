abddeed3690c3f40328649861edb2d1088b5ca4c
Noah Peden <noahpeden@gmail.com>
2017-08-19 11:45:32 -0600

    load fake data from open weather api

--- src/ClientApp.js
+++ src/ClientApp.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 import fire from './firebase';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Header from './Header';
-import Searches from './Searches'
+import Searches from './Searches';
 
-class App extends Component {
+class ClientApp extends Component {
 	constructor(props) {
 		super(props);
-
 		this.state = {
-			searches: []
+      searches: [],
+      input: '',
+      city: ''
 		};
 		this.addSearch = this.addSearch.bind(this);
+		this.getWeather = this.getWeather.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
 
 	componentWillMount() {
@@ -29,25 +33,46 @@ class App extends Component {
 	}
 
 	addSearch(e) {
+    this.setState({
+      city: this.state.input
+    })
 		e.preventDefault(); // <- prevent form submit from reloading the page
 		/* Send the message to Firebase */
 		fire.database().ref('searches').push(this.inputEl.value);
+    this.getWeather()
 		this.inputEl.value = ''; // <- clear the input
 	}
+
+	getWeather(city) {
+    const name = this.state.city
+		const APIKey = '2eacf5cd8e08d4adc524186577921400';
+		const URL = `http://api.openweathermap.org/data/2.5/weather?q=${name}&APPID=${APIKey}`;
+		axios.get(URL).then(response => console.log(response));
+  }
+  
+  handleChange(event){
+    this.setState({
+      input: event.target.value
+    })
+  }
 
 	render() {
 		return (
 			<div className="App">
 				<Header />
 				<form onSubmit={this.addSearch}>
-					<input type="text" placeholder="Search a city for the current weather" ref={el => (this.inputEl = el)} />
+					<input
+						type="text"
+            onChange={this.handleChange}
+						placeholder="Search a city for the current weather"
+						ref={el => (this.inputEl = el)}
+					/>
 					<input type="submit" />
 				</form>
-					<Link to="/History"><button>See History</button>
-          </Link>
+				<Link to="/history">See History</Link>
 			</div>
 		);
 	}
 }
 
-export default App;
+export default ClientApp;
--- src/Searches.js
+++ src/Searches.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import fire from './firebase';
 
-export default props => {
-	const { history } = props;
-	return (
-		<div>
-			{' '}{/* Render the list of searches */
-			history.map(search =>
-				<li key={search.id}>
-					{search.text}
-				</li>
-			)}
-		</div>
-	);
-};
+
+export default class Searches extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searches: []
+		};
+	}
+
+	componentWillMount() {
+		let searchesRef = fire
+			.database()
+			.ref('searches')
+			.orderByKey()
+			.limitToLast(100);
+		searchesRef.on('child_added', snapshot => {
+			let search = { text: snapshot.val(), id: snapshot.key };
+			this.setState({ searches: [search].concat(this.state.searches) });
+		});
+	}
+
+	render() {
+		return (
+			<div className="searches">
+				{/* Render the list of searches */
+				this.state.searches.map(search =>
+					<li key={search.id}>
+						{search.text}
+					</li>
+				)}
+			</div>
+		);
+	}
+}
