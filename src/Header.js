280895eff9e4176a102476c4b43b9589026ef07a
Noah Peden <noahpeden@gmail.com>
2017-08-20 19:15:02 -0600

    Add random city functionality.

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import { Link } from 'react-router-dom'
 const Wrapper = styled.div`
-  
+
 `
 
-export default () => (
+export default (props) => (
   <div>
-    This is a weather app.
+    <Link to="/history">Past Searches</Link>
+    <a onClick={props.getRandom}>Random City</a>
   </div>
 )
