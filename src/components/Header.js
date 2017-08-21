81af5f25555127b9df5fa92dd3431f8f4809f9b6
Noah Peden <noahpeden@gmail.com>
2017-08-21 07:49:31 -0600

    add transform class properties and organize src directory into components and assets

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+const Wrapper = styled.div`
+	margin-top: 50px;
+	margin-bottom: 40%;
+	color: white;
+	height: 50px;
+	width: 100%;
+	display: flex;
+	justify-content: space-around;
+	opacity: 50%;
+	a {
+		cursor: pointer;
+	}
+	a:hover {
+		text-decoration: underline;
+	}
+	div:hover {
+		text-decoration: underline;
+	}
+`;
+
+export default props =>
+	<Wrapper>
+		<Link style={{ textDecoration: 'none', color: 'white' }} to="/history">
+			<div>PAST SEARCHES</div>
+		</Link>
+		<a onClick={props.getRandom}>RANDOM</a>
+		<a onClick={props.getCurrent}>YOUR LOCATION</a>
+	</Wrapper>;
---
+++ src/components/Searches.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import fire from '../firebase';
+import styled from 'styled-components';
+
+export default class Searches extends Component {
+  state = {
+    searches: []
+  };
+  history = [];
+
+	componentWillMount() {
+		let searchesRef = fire
+			.database()
+			.ref('search')
+			.orderByKey()
+			.limitToLast(100);
+		searchesRef.on('child_added', snapshot => {
+			let searches = snapshot.val();
+			this.history.push({
+				id: snapshot.key,
+				currently: searches.currently,
+				timeStamp: searches.timeStamp,
+				description: searches.description,
+				cityName: searches.cityName
+			});
+			this.setState({ searches: this.history });
+		});
+	}
+
+	render() {
+		const Wrapper = styled.div`
+			margin-top: 30px;
+			color: white;
+			font-size: 20px;
+			font-family: sans-serif;
+			text-align: center;
+			ul {
+				margin: 10px;
+			}
+		`;
+		return (
+			<Wrapper>
+				<div className="searches">
+					<h1>PAST SEARCHES</h1>
+					{/* Render the list of searches */
+					this.state.searches.map(search =>
+						<ul key={search.id}>
+							<li>
+								{search.cityName}
+							</li>
+							<li>
+								{search.currently}
+							</li>
+							<li>
+								{search.timeStamp}
+							</li>
+							<li>
+								{search.description}
+							</li>
+						</ul>
+					)}
+				</div>
+			</Wrapper>
+		);
+	}
+}
